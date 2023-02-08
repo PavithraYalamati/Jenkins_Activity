530af2a2527f6c8124467ef61ad3258470841330
PavithraYalamati <91843530+PavithraYalamati@users.noreply.github.com>
2023-02-08 12:33:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 """
 This is a sample Jenkins Activity
 """
-##Enter into my world 
-print("Hello World!")
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
